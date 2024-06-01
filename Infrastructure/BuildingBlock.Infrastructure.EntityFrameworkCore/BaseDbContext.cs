bbd3e19ac845bda0149255cfcb3f3ea442132f2e
Phúc Phạm Hồng <phamhongphuc1403@gmail.com>
2024-06-01 20:57:27 +0700

    test CICD

--- Infrastructure/BuildingBlock.Infrastructure.EntityFrameworkCore/BaseDbContext.cs
+++ Infrastructure/BuildingBlock.Infrastructure.EntityFrameworkCore/BaseDbContext.cs
@@ -15,6 +15,8 @@ public class BaseDbContext : DbContext
     {
         _currentUser = currentUser;
         _mediator = mediator;
+        
+        
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
