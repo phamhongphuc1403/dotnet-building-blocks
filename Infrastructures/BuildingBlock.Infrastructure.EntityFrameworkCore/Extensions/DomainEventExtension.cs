cebcd56f144c63a7b09156bed226ba9ba515e1d8
Phúc Phạm Hồng <phamhongphuc1403@gmail.com>
2024-08-10 20:58:06 +0700

    [BDB-5] Rename folders to plural

--- Infrastructures/BuildingBlock.Infrastructure.EntityFrameworkCore/Extensions/DomainEventExtension.cs
+++ Infrastructures/BuildingBlock.Infrastructure.EntityFrameworkCore/Extensions/DomainEventExtension.cs
@@ -8,7 +8,8 @@ public static class DomainEventExtension
 {
     public static async Task DispatchDomainEventsAsync(this IMediator mediator, DbContext dbContext)
     {
-        var domainEntities = dbContext.ChangeTracker.Entries<AggregateRoot>().Where(x => x.Entity.DomainEvents.Count != 0)
+        var domainEntities = dbContext.ChangeTracker.Entries<AggregateRoot>()
+            .Where(x => x.Entity.DomainEvents.Count != 0)
             .ToList();
 
         var domainEvents = domainEntities.SelectMany(x => x.Entity.DomainEvents).ToList();
