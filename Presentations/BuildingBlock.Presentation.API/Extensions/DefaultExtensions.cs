c0f7a8f0f46e14e2d70d180b33e438057c32a1d5
Phúc Phạm Hồng <phamhongphuc1403@gmail.com>
2024-08-11 18:35:34 +0700

    [BDB-6] Combine DefaultModuleExtensions.cs with DefaultExtensions.cs

--- Presentations/BuildingBlock.Presentation.API/Extensions/DefaultExtensions.cs
+++ Presentations/BuildingBlock.Presentation.API/Extensions/DefaultExtensions.cs
@@ -1,4 +1,8 @@
 using System.Text.Json.Serialization;
+using BuildingBlock.Core.Application;
+using BuildingBlock.Core.Domain;
+using BuildingBlock.Infrastructure.EntityFrameworkCore;
+using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,8 +10,11 @@ namespace BuildingBlock.Presentation.API.Extensions;
 
 public static class DefaultExtensions
 {
-    public static IServiceCollection AddDefaultExtensions(this IServiceCollection services,
-        IConfiguration configuration)
+    public static async Task<IServiceCollection> AddDefaultExtensions<TApplicationAssemblyReference,
+        TDomainAssemblyReference, TDbContext>(this IServiceCollection services, IConfiguration configuration)
+        where TDbContext : BaseDbContext
+        where TApplicationAssemblyReference : ApplicationAssemblyReference
+        where TDomainAssemblyReference : DomainAssemblyReference
     {
         services
             .AddControllers()
@@ -20,10 +27,25 @@ public static class DefaultExtensions
             .AddApplicationCors(configuration)
             .AddHttpContextAccessor()
             .AddCurrentUser()
+            .AddDatabase<TDbContext>(configuration)
+            .AddMapper<TDbContext>()
+            .AddCqrs<TApplicationAssemblyReference, TDomainAssemblyReference>()
             .AddDefaultOpenApi(configuration)
             .AddEventBus(configuration)
-            .AddInMemoryCache(configuration)
-            .AddEmail(configuration);
+            .AddValidatorsFromAssembly(typeof(TApplicationAssemblyReference).Assembly)
+            .AddInMemoryCache(configuration);
+
+        services
+            .RegisterSeeders<TApplicationAssemblyReference>()
+            .RegisterServices<TApplicationAssemblyReference>()
+            .RegisterServices<TDomainAssemblyReference>()
+            .RegisterDefaultRepositories<TDomainAssemblyReference, TDbContext>()
+            .RegisterCustomRepositories<TDbContext>()
+            .RegisterCachedRepositories<TDbContext>()
+            .RegisterIntegrationEventHandlers<TApplicationAssemblyReference>()
+            .RegisterUnitOfWork<TDbContext>();
+
+        await services.ApplyMigrationAsync<TDbContext>();
 
         return services;
     }
